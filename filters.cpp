b13040cbd7c242bc8423bebc3ca63f4ec42474e0
David Lopez <dnlm92@me.com>
2014-05-15 18:25:21 -0500

    [MODIFY] filters.cpp optimization

--- filters.cpp
+++ filters.cpp
@@ -1,96 +1,93 @@
-//#include "lodepng.h"
 #include "filters.h"
-//#include <iostream>
 
-void decodeOneStep(const char* filename, std::vector<unsigned char> *image, unsigned *width, unsigned *height)
+void decodeOneStep(const char* filename,
+                   std::vector<unsigned char> *image,
+                   unsigned *width,
+                   unsigned *height)
 {
-  //decode
-  unsigned error = lodepng::decode(*image, *width, *height, filename);
+	// decode
+	unsigned error = lodepng::decode(*image, *width, *height, filename);
 
-  //if there's an error, display it
-  if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+	// if there's an error, display it
+	if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 
-  //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+	// the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
 }
 
-void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height)
+void encodeOneStep(const char* filename,
+                   std::vector<unsigned char>& image,
+                   unsigned width,
+                   unsigned height)
 {
-  //Encode the image
-  unsigned error = lodepng::encode(filename, image, width, height);
+	// Encode the image
+	unsigned error = lodepng::encode(filename, image, width, height);
 
-  //if there's an error, display it
-  if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+	// if there's an error, display it
+	if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 }
 
-void sepiaFilter(std::vector<unsigned char> *image, unsigned width, unsigned height){
+void sepiaFilter(std::vector<unsigned char> *image,
+                 unsigned width,
+                 unsigned height)
+{
 	float inRed, inGreen, inBlue;
-	for(unsigned y = 0; y < height*4; y++)
-		#pragma vector always
-		for(unsigned x = 0; x < width*4; x++)
+	for(unsigned y = 0; y < height; y++)
+		#pragma simd
+		for(unsigned x = 0; x < width; x++)
 		{
-			int pos = width * y + x;
+			int pos = ( width * y + x ) * 4;
 			float newColor;
-			try{
-				switch (pos % 4) {
-					//RED
-					case 0:
-						inRed = image->at(pos);
-						inGreen = image->at(pos+1);
-						inBlue = image->at(pos+2);
-						newColor = (inRed * .393) + (inGreen * .769) + (inBlue * .189) - 30;
-						if (newColor > 255) {
-							image->at(pos) = 255;
-						}
-						else if (newColor < 0) {
-							image->at(pos) = 0;
-						}
-						else {
-							image->at(pos) = newColor;
-						}
-						
-						break;
-					//GREEN
-					case 1:
-						newColor = (inRed * .349) + (inGreen * .686) + (inBlue * .168) - 30;
-						if (newColor > 255) {
-							image->at(pos) = 255;
-						}
-						else if (newColor < 0) {
-							image->at(pos) = 0;
-						}
-						else {
-							image->at(pos) = newColor;
-						}
-						break;
-					//BLUE
-					case 2:
-						newColor = (inRed * .272) + (inGreen * .534) + (inBlue * .131) - 30;
-						if (newColor > 255) {
-							image->at(pos) = 255;
-						}
-						else if (newColor < 0) {
-							image->at(pos) = 0;
-						}
-						else {
-							image->at(pos) = newColor;
-						}
-						break;
-				}
+			inRed = image->at(pos);
+			inGreen = image->at(pos+1);
+			inBlue = image->at(pos+2);
+		//RED
+			newColor = (inRed * .393) + (inGreen * .769) + (inBlue * .189) - 30;
+			if (newColor > 255) {
+				image->at(pos) = 255;
 			}
-			catch(std::out_of_range e){
+			else if (newColor < 0) {
+				image->at(pos) = 0;
+			}
+			else {
+				image->at(pos) = newColor;
+			}
+
+		//GREEN
+			newColor = (inRed * .349) + (inGreen * .686) + (inBlue * .168) - 30;
+			if (newColor > 255) {
+				image->at(pos + 1) = 255;
+			}
+			else if (newColor < 0) {
+				image->at(pos + 1) = 0;
+			}
+			else {
+				image->at(pos + 1) = newColor;
+			}
+		//BLUE
+			newColor = (inRed * .272) + (inGreen * .534) + (inBlue * .131) - 30;
+			if (newColor > 255) {
+				image->at(pos + 2) = 255;
+			}
+			else if (newColor < 0) {
+				image->at(pos + 2) = 0;
+			}
+			else {
+				image->at(pos + 2) = newColor;
 			}
 		}
 }
 
-void grayFilter(std::vector<unsigned char> *image, unsigned width, unsigned height){
+void grayFilter(std::vector<unsigned char> *image,
+                unsigned width,
+                unsigned height)
+{
 	float inRed, inGreen, inBlue;
-	for(unsigned y = 0; y < height*4; y++)
-		//#pragma vector always
-		for(unsigned x = 0; x < width*4; x = x + 4)
+	for(unsigned y = 0; y < height; y++)
+		#pragma simd
+		for(unsigned x = 0; x < width; x++)
 		{
-			int pos = width * y + x;
+			int pos = ( width * y + x ) * 4 ;
 			float newColor;
-			try{
 				inRed = image->at(pos);
 				inGreen = image->at(pos+1);
 				inBlue = image->at(pos+2);
@@ -98,19 +95,5 @@ void grayFilter(std::vector<unsigned char> *image, unsigned width, unsigned heig
 				image->at(pos) = newColor;
 				image->at(pos + 1) = newColor;
 				image->at(pos + 2) = newColor;
-			}
-			catch(std::out_of_range e){
-			}
 		}
 }
-
-// int main(int argc, char *argv[])
-// {
-// 	const char* filename = argc > 1 ? argv[1] : "test.png";
-// 	std::vector<unsigned char> image; //the raw pixels
-// 	unsigned width, height;
-// 	decodeOneStep(filename, &image, &width, &height);
-// 	grayFilter(&image, width, height);
-// 	encodeOneStep(filename, image, width, height);
-
-// }
