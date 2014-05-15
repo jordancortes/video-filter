1b5517049efa84ce5b2606f967a74a4f1fb1c91b
Jordan Cortes Guzman <jordancg91@gmail.com>
2014-05-15 18:00:29 -0500

    Threads are incorporated for image filters
    p_threads

--- video-filter.cpp
+++ video-filter.cpp
@@ -7,6 +7,15 @@
 #define SUCCESS 0
 #define ERROR   1
 #define THREADS 1
+#define MT_BEGIN 0
+#define MT_END  1
+#define SEPIA   0
+#define BN      1
+
+int main_thread_status = MT_BEGIN;
+pthread_mutex_t mutex_frame;
+int actual_frame = 1;
+int filter_type;
 
 int checkParameters(Jzon::Object params)
 {
@@ -173,6 +182,25 @@ void compileVideo(std::string filename, float frames)
     exec(extract_frames_cmd.c_str());
 }
 
+void *applyFilter(void *threadID)
+{
+    long tID;
+    double this_frame;
+    tID = (long)threadID;
+
+    while(main_thread_status != MT_END)
+    {
+        // Lock a mutex prior to updating the value
+        pthread_mutex_lock (&mutex_frame);
+        this_frame = actual_frame++;
+        pthread_mutex_unlock (&mutex_frame);
+
+        printf("%lf\n", this_frame);
+    }
+
+    pthread_exit(NULL);
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -180,6 +208,10 @@ int main(int argc, char* argv[])
     float duration = 0.0;
     float frames = 0.0;
     std::string filetype = "";
+    pthread_t threads[THREADS];
+    pthread_attr_t attr;
+    int thread_result;
+    void *status;
 
     // Read parameters from the JSON file
     Jzon::FileReader::ReadFile("params.json", params);
@@ -193,10 +225,38 @@ int main(int argc, char* argv[])
     // Grab the neccesary info from the video file
     checkVideInfo(params, &duration, &frames, &filetype);
 
+    pthread_mutex_init(&mutex_frame, NULL);
+
+    // Initialize and set thread detached attribute
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    // With p_threads calls the filter method for each image
+    for (int i = 0; i < THREADS; i++)
+    {
+        thread_result = pthread_create(&threads[i],
+                                       &attr,
+                                       applyFilter,
+                                       NULL);
+    }
+
     // Get frames of video
     extractFrames(params.Get("filename").ToString(), frames);
 
+    // Set the end of the frames
+    main_thread_status = MT_END;
+
+    /* Free attribute and wait for the other threads */
+    pthread_attr_destroy(&attr);
+    for (int i = 0; i < THREADS; i++)
+    {
+        thread_result = pthread_join(threads[i], &status);
+    }
+
+    // Recompile the video
     compileVideo("./resources/output." + filetype, frames);
 
-    return SUCCESS;
+    /* Last thing that main() should do */
+    pthread_mutex_destroy(&mutex_frame);
+    pthread_exit(NULL);
 }
